b035c2faf4ec387cbdb7c0a77b5676ed3e2023ee
lim <doolim98@gmail.com>
2021-11-30 22:45:43 +0900

    test

---
+++ src/Assets/FleaScripts/Test/TestDB.cs
@@ -0,0 +1,14 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common.Interfaces;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class TestDB : UdonSharpBehaviour
+{
+    void Start()
+    {
+    }
+}
