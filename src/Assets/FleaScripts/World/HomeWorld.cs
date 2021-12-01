6745babfa8a9bd7e16257c62e758dfb14f4dcfe7
lim <doolim98@gmail.com>
2021-12-01 21:16:55 +0900

    nolfs

---
+++ src/Assets/FleaScripts/World/HomeWorld.cs
@@ -0,0 +1,29 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class HomeWorld : UdonSharpBehaviour
+{
+    FleaCore core;
+    public GameObject RegisterPanel;
+    public GameObject LoginPanel;
+    public GameObject HomePanel;
+    void Start()
+    {
+        core = GetComponent<FleaCore>();
+        RegisterPanel.SetActive(false);
+        LoginPanel.SetActive(false);
+        HomePanel.SetActive(true);
+    }
+
+    public void OnClickRegister()
+    {
+    }
+    public void OnClickLogin()
+    {
+    }
+
+}
